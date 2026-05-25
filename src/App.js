f3148f5c25f8c5859e76736c1a48d1318f98f1c1
Vinaya-16 <vinayapatole16@gmail.com>
2026-05-25 13:32:22 +0530

    Frontend Based TODOS Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [newtask, setTask] = useState("");
+  const [tasks, setTasks] = useState([
+    // { id: 1, text: "Finish project proposal", completed: false },
+    // { id: 2, text: "Buy groceries", completed: false },
+    // { id: 3, text: "Go for a run", completed: false }
+  ]);
+
+  const addTask = () => {
+
+    if (newtask.trim() === "") {
+      alert("Please enter a valid task");
+      return;
+    }
+
+    const newTask = {
+      id: Date.now(),
+      text: newtask,
+      completed: false
+    };
+
+    setTasks([...tasks, newTask]);
+    setTask("");
+  };
+
+  const toggleComplete = (idToToggle) => {
+    setTasks(tasks.map(t =>
+      t.id === idToToggle ? { ...t, completed: !t.completed } : t
+    ));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="todo-card">
+        <h1>Todo List</h1>
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="Enter task"
+            value={newtask}
+            onChange={(e) => setTask(e.target.value)}
+          />
+          <button className="add-btn" onClick={addTask}> Add </button>
+        </div>
+        <ul className="task-list">
+          {tasks.map((item) => (
+            <li key={item.id} className="task-item">
+              <label className="checkbox-container">
+                <input
+                  type="checkbox"
+                  checked={item.completed}
+                  onChange={() => toggleComplete(item.id)}
+                />
+                <span className={`task-text ${item.completed ? 'completed' : ''}`}>{item.text}</span>
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
